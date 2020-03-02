a521ffdcb2f1f106debbc0b4b9856b9363cb937d
Gnimuc <qiyupei@gmail.com>
2020-03-02 21:11:07 +0800

    Try to fix #3

--- src/crypto.jl
+++ src/crypto.jl
@@ -1,7 +1,8 @@
 # helper functions for crypto_
-function crypto_box_seal(msg, pk)
+function LibSodium.crypto_box_seal(msg, pk)
     len = sizeof(msg)
     ciphertext = Vector{Cuchar}(undef, crypto_box_SEALBYTES + len)
-    crypto_box_seal(ciphertext, msg, len, pk)
-    return String(ciphertext)
+    result = crypto_box_seal(ciphertext, msg, len, pk)
+    result == 0 || error("failed to seal box!")
+    return ciphertext
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,14 @@ end
 
 @testset "Sealed boxes" begin
     msg = "Message"
-    msg_len = length(msg)
+    msg_len = sizeof(msg)
     ciphertext_len = crypto_box_SEALBYTES + msg_len
     recipient_pk = Vector{Cuchar}(undef, crypto_box_PUBLICKEYBYTES)
     recipient_sk = Vector{Cuchar}(undef, crypto_box_SECRETKEYBYTES)
     @test crypto_box_keypair(recipient_pk, recipient_sk) == 0
     ciphertext = Vector{Cuchar}(undef, ciphertext_len)
     @test crypto_box_seal(ciphertext, msg, msg_len, recipient_pk) == 0
+    @test crypto_box_seal(msg, recipient_pk) == ciphertext
     decrypted = Vector{Cuchar}(undef, msg_len)
     @test crypto_box_seal_open(decrypted, ciphertext, ciphertext_len, recipient_pk, recipient_sk) == 0
     @test String(decrypted) == msg
