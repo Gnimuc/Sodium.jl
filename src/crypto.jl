ae3a6f99a8fd54d1b427367450d1aa8bd098dc51
Aaron R. Matthis <rapus95@gmail.com>
2020-03-02 14:29:58 +0100

    fixup convenience function to create a sealed box; fixes #3
    rename crypto_box_seal to seal
    return a base64 encoded string instead of a string from bytes
    add docs
    bump minor version to 1.1.0

--- src/crypto.jl
+++ src/crypto.jl
@@ -1,7 +1,16 @@
+using Base64
 # helper functions for crypto_
-function crypto_box_seal(msg, pk)
+"""
+    seal(message, publickey)
+
+Takes a message of any type which has a proper sizeof (to get proper C references)
+and a public key from the remote.
+All remaining arguments to call `libsodium.crypto_box_seal` are inferred automatically
+and the result is returned as a base64 encoded string.
+"""
+function seal(msg, pk)
     len = sizeof(msg)
     ciphertext = Vector{Cuchar}(undef, crypto_box_SEALBYTES + len)
     crypto_box_seal(ciphertext, msg, len, pk)
-    return String(ciphertext)
+    return base64encode(ciphertext)
 end
